044f96e9e2a22e4cdb50956dded5c59a8aea2640
kittituch <kittituch.dev@gmail.com>
2025-01-10 08:19:17 +0700

    first commit

---
+++ src/main/java/com/codekit/moneykit/MoneykitApplication.java
@@ -0,0 +1,13 @@
+package com.codekit.moneykit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneykitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneykitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codekit/moneykit/MoneykitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codekit.moneykit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneykitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
